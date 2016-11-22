8e2a02064ca5e3a6ca9369c5b035bed7714ff1fe
Gunhoon Lee <gunhoon@gmail.com>
2016-11-23 02:36:54 +0900

    Implement the script for building and installing boost

---
+++ tools/boost_build.py
@@ -0,0 +1,116 @@
+# Copyright (c) 2016, Gunhoon Lee (gunhoon@gmail.com)
+# All rights reserved.
+
+import os
+import sys
+import subprocess
+from optparse import OptionParser
+
+
+# Set the boost version to be installed.
+boost_version = '1.62.0'
+
+
+# Helper funtions
+def run(cmd, path):
+  """ Run a command. """
+  sys.stdout.write('--- Running "%s" in "%s"...\n' % (cmd, path))
+  args = cmd.split()
+  return subprocess.check_call(args, cwd=path, shell=(sys.platform == 'win32'))
+
+
+##
+# Program entry point.
+##
+
+# Can not be loaded as a module.
+if __name__ != "__main__":
+  sys.stderr.write('*** ERROR: This file can not be loaded as a module!')
+  sys.exit()
+
+
+# Determine the platform.
+if sys.platform.startswith('linux'):
+  platform = 'linux'
+elif sys.platform == 'win32':
+  platform = 'windows'
+else:
+  sys.stderr.write('*** ERROR: Unsupported platform(%s)...' % sys.platform)
+  sys.exit()
+
+
+# Parse command-line options
+disc = """
+This script builds and installs the Boost C++ libraries.
+"""
+
+parser = OptionParser(description=disc)
+parser.add_option('--debug-build',
+                  action='store_true', dest='debug_build', default=False,
+                  help='Perform not release build, but debug build.')
+(options, args) = parser.parse_args()
+
+if options.debug_build:
+  build_type = 'debug'
+else:
+  build_type = 'release'
+
+sys.stdout.write('--- build type : %s\n\n' % build_type)
+
+
+# base directories
+tools_dir = os.path.dirname(__file__)
+root_dir = os.path.join(tools_dir, os.pardir)
+boost_dir = os.path.join(root_dir, 'boost_' + boost_version.replace('.', '_'))
+
+sys.stdout.write('--- tools directory : "%s"\n' % os.path.abspath(tools_dir))
+sys.stdout.write('--- root directory  : "%s"\n' % os.path.abspath(root_dir))
+sys.stdout.write('--- boost directory : "%s"\n\n' % os.path.abspath(boost_dir))
+
+
+# Check that the boost directory exists.
+if not os.path.isdir(boost_dir):
+  sys.stderr.write('*** ERROR: "%s" directory does not exist...' % boost_dir)
+  sys.exit()
+
+
+# install directory ("/usr/local" on Linux, "{root_dir}/install" on Windows)
+if platform == 'linux':
+  install_root_path = os.path.abspath('/usr/local')
+elif platform == 'windows':
+  install_root_path = os.path.join(root_dir, 'install')
+
+install_library_path = os.path.join(install_root_path, 'lib', 'boost')
+install_include_path = os.path.join(install_root_path, 'include')
+
+sys.stdout.write('--- installing root path : "%s"\n' % install_root_path)
+sys.stdout.write('--- installing library path : "%s"\n' % install_library_path)
+sys.stdout.write('--- installing include path : "%s"\n' % install_include_path)
+
+
+# Prepare Boost for building.
+if platform == 'linux':
+  shell_cmd = './bootstrap.sh'
+elif platform == 'windows':
+  shell_cmd = 'bootstrap.bat'
+
+first_cmd = '%s --prefix=%s --libdir=%s --includedir=%s' \
+    % (shell_cmd, install_root_path, install_library_path, install_include_path)
+run(first_cmd, boost_dir)
+
+
+# Build and install Boost
+if platform == 'linux':
+  shell_cmd = 'sudo ./b2'
+elif platform == 'windows':
+  shell_cmd = 'b2'
+
+second_cmd = '%s variant=%s link=shared threading=multi install' \
+    % (shell_cmd, build_type)
+run(second_cmd, boost_dir)
+
+sys.stdout.write('--- Installed root path : "%s"\n' % install_root_path)
+sys.stdout.write('--- Installed library path : "%s"\n' % install_library_path)
+sys.stdout.write('--- Installed include path : "%s"\n' % install_include_path)
+sys.stdout.write('--- Boost build and installation are completed...\n')
+
---
+++ tools/boost_build.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python $(dirname $0)/$(basename $0 .sh).py $@
+
